ec2b5abdde182654ed8acebf22245df1fa52614a
andruhus <aaademed@gmail.com>
2020-12-23 15:34:35 +0200

    Added Car class

---
+++ source/Car.py
@@ -0,0 +1,8 @@
+
+class Car:
+    def __init__(self,name,manufacturer,year,car_usage,history_of_defects):
+        self.name = name
+        self.manufacturer = manufacturer
+        self.car_usage = car_usage
+        self.history_of_defects = history_of_defects
+        pass
