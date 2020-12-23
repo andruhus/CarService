6e884fec4818d074c100f7ace2fc54a910ad2f31
andruhus <aaademed@gmail.com>
2020-12-23 16:07:47 +0200

    Added Probability Calculator

--- source/Car.py
+++ source/Car.py
@@ -3,6 +3,7 @@ class Car:
     def __init__(self,name,manufacturer,year,car_usage,history_of_defects):
         self.name = name
         self.manufacturer = manufacturer
+        self.year = year
         self.car_usage = car_usage
         self.history_of_defects = history_of_defects
         pass
---
+++ source/ProbabilityCalculator.py
@@ -0,0 +1,42 @@
+class PrCalcBase:
+    def __init__(self,b,k,deffect_name):
+        self.k_coef = k
+        self.b_coef = b
+        self.deffect_name = deffect_name
+        pass
+
+    def FindProb(self,car):
+        pass
+    def FindNumDeffects(self,car):
+        res = 0
+        for deffect in car.history_of_defects:
+            if deffect.name == self.deffect_name:
+                res += 1
+                pass
+        return res
+    def CheckRes(self,res):
+        if res < 0:
+            res = 0
+        if res > 1:
+            res = 1
+        return res
+
+class PrCalcFix(PrCalcBase):
+    def FindProb(self,car):
+        return self.b_coef
+
+class PrCalcTime(PrCalcBase):
+    def FindProb(self,car):
+        res =  self.k_coef * car.year + self.b_coef
+        return self.CheckRes(res)
+
+class PrCalcUsage(PrCalcBase):
+    def FindProb(self,car):
+        res = self.k_coef * car.year + self.b_coef
+        return self.CheckRes(res)
+
+class PrCalcHist(PrCalcBase):
+    def FindProb(self,car):
+        parameter = self.FindNumDeffects(car)
+        res = self.k_coef * parameter + self.b_coef
+        return self.CheckRes(res)
